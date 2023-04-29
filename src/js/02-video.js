afa2a958899404c8dd4332a025d5c39af34348dc
Dmytro Petyshyn <dev6012@gmail.com>
2023-04-29 19:52:26 +0300

    task 2 complited

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,11 +4,32 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
+const save = (key, value) => {
+  try {
+    const serializedState = JSON.stringify(value);
+    localStorage.setItem(key, serializedState);
+  } catch (error) {
+    console.error('Set state error: ', error.message);
+  }
+};
+
+const load = key => {
+  try {
+    const serializedState = localStorage.getItem(key);
+    return serializedState === null ? undefined : JSON.parse(serializedState);
+  } catch (error) {
+    console.error('Get state error: ', error.message);
+  }
+};
+
+function setSavedTime() {
+  const timeObj = load('videoplayer-current-time');
+  player.setCurrentTime(timeObj.seconds);
+}
+
 player.on(
   'timeupdate',
-  throttle(time => setToStorage(time), 1000)
+  throttle(time => save('videoplayer-current-time', time), 1000)
 );
 
-function setToStorage(t) {
-  localStorage.setItem('playback-time', JSON.stringify(t));
-}
+document.addEventListener('DOMContentLoaded', setSavedTime);
